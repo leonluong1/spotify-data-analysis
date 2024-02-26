c90e43e0a8f42f92915f196d6c96f12c5fc98122
AhldenBrough <50213731+AhldenBrough@users.noreply.github.com>
2024-02-25 17:58:20 -0800

    successfully sending query to /buildquery route

--- app.js
+++ app.js
@@ -9,13 +9,18 @@ const PORT = process.env.PORT || 3000;
 const {
     querySongs
  } = require('./models/db.js');
-
+ const bodyParser = require('body-parser');
+ app.use(bodyParser.json());
+const {
+    buildQuery
+ } = require('./static/js/buildQuery.js');
  
 app.use(express.static(path.join(__dirname)));
 app.use('/', homeRouter);
 app.use('/model', modelRouter);
 app.use('/dashboard', tableauRouter);
 app.use('/exploration', explorationRouter);
+
 
 /*app.use('/images', express.static('images'));
 app.use('/static', express.static('static'));
@@ -32,10 +37,23 @@ app.get('/api/songs', async (req, res) => {
     res.json(query);
 });
 
+app.post('/buildQuery', (req, res) => {
+    console.log(req);
+    console.log('gets to app.js');
+    const { acousticness, danceability, energy, instrumentalness, liveness, loudness, speechiness, tempo, valence, start_year, end_year } = req.body;
+  
+    const query = buildQuery(acousticness, danceability, energy, instrumentalness, liveness, loudness, speechiness, tempo, valence, start_year, end_year);
+
+    res.json({ query });
+  });
+  
+
 app.use((err, req, res, next) => {
+    console.log(req)
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
