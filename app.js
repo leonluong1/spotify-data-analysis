756b1d3a44e39f43884a515ba376a870cdab7f38
leonluong1 <leon1838luong@yahoo.com>
2024-02-27 23:15:01 -0800

    finishing playlists page

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ const playlistsRouter = require('./routes/playlists.js');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+
 const {
     querySongs
  } = require('./models/db.js');
- const bodyParser = require('body-parser');
- app.use(bodyParser.json());
+
+const bodyParser = require('body-parser');
+//app.use(bodyParser.json());
+
 const {
     buildQuery
  } = require('./static/js/buildQuery.js');
@@ -28,20 +31,25 @@ app.use('/resources', resourcesRouter);
 app.use('/playlists', playlistsRouter);
 
 
-/*app.use('/images', express.static('images'));
-app.use('/static', express.static('static'));
-app.use('/', express.static('css', {
-    setHeaders: (res) => {
-        res.setHeader('Content-Type', 'text/css');
-    },
-}));*/
+
+const columns = "track_name, album_name, principal_artist_name, artist_genres, principal_artist_followers, duration_ms, duration_min, popularity,\
+popularity, acousticness, danceability, energy, instrumentalness, key, liveness, loudness, mode, speechiness, tempo, valence, year"
+const names = ["ahlden", "daniel", "immanuel", "martin", "leon", "brian"];
+const ahlden =  [12, 7911, 9976, 303, 9739];
+const daniel =  [314, 507, 6134, 347, 4519, 2514, 2203, 9357, 5892, 3281];
+const immanuel =  [5209, 8838, 9497, 9708, 10883];
+const martin =  [18, 29, 79, 85, 4945, 9412, 9411, 10471, 10478, 10496];
+const leon =  [5212, 5528, 5559, 2415, 4684, 8518, 7611, 4336, 8032, 10633];
+const brian =  [2116, 8737, 4318, 5228, 4027, 3519, 2594, 4016, 4320, 798];
+const playlists = [ahlden, daniel, immanuel, martin, leon, brian]
 
 
-app.get('/api/songs', async (req, res) => {
-    let query = await querySongs()
-    console.log(res);
-    res.json(query);
-});
+for (let i in names) {
+    app.get(`/songs/${names[i]}`, async (req, res) => {
+        let query = await querySongs(`select ${columns}  from song where id in ${'(' + playlists[i].join(', ') + ')'}`);
+        res.json(query);
+      });
+}
 
 app.post('/buildQuery', (req, res) => {
     console.log(req);
@@ -59,8 +67,9 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
-
+  
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
 
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-// Tableau page route
+// Dashboard page route
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views', 'dashboard.html'));
 });
